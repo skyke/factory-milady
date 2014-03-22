834f9291fa7550201ea0bcab579d697597cfd06f
Niels Clauwers <nclauwers@gmail.com>
2014-03-22 03:26:59 +0100

    first changes to code

--- README.md
+++ README.md
@@ -11,15 +11,23 @@ Factory-milady is a factory library for [Node.js](http://nodejs.org/) inspired
 
  So for a full explanation please see [here](https://github.com/petejkim/factory-lady).
 
- ## Issues with factory-lady:
+## Issues with factory-lady:
 
 1. The library only works with models which support the `new` keyword and
 `save` method to persist the data. But what if you just wanted to have a
 literal object returned?
 
 2. The code isn't always following the node callback convention `callback
-(err, result)`. When an error occurs during the save method the code throws an
-Exception instead of calling the callback function with the error.
+(err, result)`. When an error occurs during the `save` method the code throws
+ an exception instead of calling the callback function with the error.
+My guess is that the throw is implemented to support the inline `assoc`
+function. The `assoc` function will trigger a `save` and thus errors are
+possible.
+For this reason the `assoc` function will not trigger a `save` but will
+trigger a `build`.
+
+Note: I only enforce callback convention for `Factory.create` because a
+`save` is triggered. `Create` and `Object` don't need this.
 
 ## Installation
 
@@ -34,22 +42,28 @@ npm install factory-milady
 JavaScript:
 
 ```javascript
-var Factory = require('factory-milady')
-  , User    = require('../../app/models/user')
-  , Post    = require('../../app/models/post');
+var Factory        = require('factory-milady');
+var User           = require('../../app/models/user');
+var Post           = require('../../app/models/post');
 
 var emailCounter = 1;
 
 Factory.define('user', User, {
-  email    : function(cb) { cb('user' + emailCounter++ + '@example.com'); } // lazy attribute
-, state    : 'activated'
-, password : '123456'
+  email    : function(cb) { cb('user' + emailCounter++ + '@example.com'); }, // lazy attribute
+  state    : 'activated',
+  password : '123456'
 });
 
 Factory.define('post', Post, {
-  user_id  : Factory.assoc('user', 'id') // simply Factory.assoc('user') for user object itself
-, subject  : 'Hello World'
-, content  : 'Lorem ipsum dolor sit amet...'
+  user_id  : Factory.assoc('user', 'id'), // simply Factory.assoc('user') for user object itself
+  subject  : 'Hello World',
+  content  : 'Lorem ipsum dolor sit amet...'
+});
+
+// You can define without passing a model it's actually the same as passing *function() {}*
+Factory.define('appointment', {
+  what  : 'meeting',
+  where : 'Brussels'
 });
 ```
 
@@ -66,13 +80,25 @@ Factory.build('post', { title: 'Foo', content: 'Bar' }, function(post) {
   // build a post and override title and content
 });
 
-Factory.create('post', function(post) {
+Factory.build('appointment', function(appointment) {
+  // appointment is an instantiated object from an empty constructor
+});
+
+Factory.create('post', function(err, post) {
   // post is a saved Post instance
+});
+
+Factory.create('appointment', function(err, appointment) {
+  // err will not be null because save is not supported
 });
 
 Factory('post', function(post) {
   // post is a saved Post instance
   // same as Factory.create
+});
+
+Factory.object('appointment', function(err, appointment) {
+  // appointment is an object literal
 });
 ```
 
--- test/factory-test.js
+++ test/factory-test.js
@@ -5,112 +5,143 @@ var context = describe;
 describe('Factory', function() {
   var Model, Person, Job;
 
-  before(function() {
-    Model = function() {};
+    before(function() {
+        Model = function() {};
 
-    Model.prototype.save = function(callback) {
-      this.saveCalled = true;
-      callback();
-    };
+        Model.prototype.save = function(callback) {
+            this.saveCalled = true;
+            callback();
+        };
 
-    Person = function() {};
-    Person.protoype = new Model();
+        Person = function() {};
+        Person.protoype = new Model();
 
-    Job = function() {};
-    Job.prototype = new Model();
-  });
-
-  beforeEach(function() {
-    var nameCounter = 1;
-
-    Factory.define('person', Person, {
-      name: function(cb) { cb("person " + nameCounter++); }
-    , age: 25
-    , job: Factory.assoc('job')
-    , title: Factory.assoc('job', 'title')
+        Job = function() {};
+        Job.prototype = new Model();
     });
 
-    Factory.define('job', Job, {
-      title: 'Engineer'
-    , company: 'Foobar Inc.'
-    });
-  });
+    beforeEach(function() {
+        var nameCounter = 1;
 
-  describe('#build', function() {
-    it('builds, but does not save the object', function(done) {
-      Factory.build('job', function(job) {
-        (job instanceof Job).should.be.true;
-        job.title.should.eql('Engineer');
-        job.company.should.eql('Foobar Inc.');
-        job.should.not.have.property('saveCalled');
-        done();
-      });
-
-      context('passing attributes as second argument', function() {
-        it('sets them', function(done) {
-          Factory.build('job', { title: "Artist", company: "Bazqux Co." }, function(job) {
-            (job instanceof Job).should.be.true;
-            job.title.should.eql('Artist');
-            job.company.should.eql('Bazqux Co.');
-            job.should.not.have.property('saveCalled');
-            done();
-          });
+        Factory.define('appointment', {
+            where: 'Hasselt',
+            when: 'tomorrow'
         });
-      });
 
-      context('factory containing an association', function() {
-        it('is able to handle that', function(done) {
-          Factory.build('person', { age: 30 }, function(person) {
-            (person instanceof Person).should.be.true;
-            person.should.not.have.property('saveCalled');
-            person.name.should.eql('person 1');
-            person.age.should.eql(30);
-            (person.job instanceof Job).should.be.true;
-            person.job.title.should.eql('Engineer');
-            person.job.company.should.eql('Foobar Inc.');
-            person.job.saveCalled.should.be.true;
-            person.title.should.eql('Engineer');
-            done();
-          });
+        Factory.define('person', Person, {
+              name: function(cb) { cb("person " + nameCounter++); }
+            , age: 25
+            , job: Factory.assoc('job')
+            , title: Factory.assoc('job', 'title')
         });
-      });
-    });
-  });
 
-  describe('#create', function() {
-    it('builds and saves the object', function(done) {
-      Factory.create('job', function(job) {
-        (job instanceof Job).should.be.true;
-        job.title.should.eql('Engineer');
-        job.company.should.eql('Foobar Inc.');
-        job.saveCalled.should.be.true;
-        done();
-      });
-    });
-
-    context('passing attributes as second argument', function() {
-      it('sets them', function(done) {
-        Factory.create('job', { title: "Artist", company: "Bazqux Co." }, function(job) {
-          (job instanceof Job).should.be.true;
-          job.title.should.eql('Artist');
-          job.company.should.eql('Bazqux Co.');
-          job.saveCalled.should.be.true;
-          done();
+        Factory.define('job', Job, {
+              title: 'Engineer'
+            , company: 'Foobar Inc.'
         });
-      });
     });
 
-    context('Factory(...) instead of Factory.create(...)', function() {
-      it('is aliased, so it does the same thing as #create', function(done) {
-        Factory('job', function(job) {
-          (job instanceof Job).should.be.true;
-          job.title.should.eql('Engineer');
-          job.company.should.eql('Foobar Inc.');
-          job.saveCalled.should.be.true;
-          done();
+    describe('#build', function() {
+        it('builds, but does not save the object', function(done) {
+            Factory.build('job', function(job) {
+                (job instanceof Job).should.be.true;
+                job.title.should.eql('Engineer');
+                job.company.should.eql('Foobar Inc.');
+                job.should.not.have.property('saveCalled');
+                done();
+            });
         });
-      });
+
+        it('passing attributes as second argument', function(done) {
+            Factory.build('job', { title: "Artist", company: "Bazqux Co." }, function(job) {
+                (job instanceof Job).should.be.true;
+                job.title.should.eql('Artist');
+                job.company.should.eql('Bazqux Co.');
+                job.should.not.have.property('saveCalled');
+                done();
+            });
+        });
+
+        it('factory containing an association', function(done) {
+            Factory.build('person', { age: 30 }, function(person) {
+                (person instanceof Person).should.be.true;
+                person.should.not.have.property('saveCalled');
+                person.name.should.eql('person 1');
+                person.age.should.eql(30);
+                (person.job instanceof Job).should.be.true;
+                person.job.title.should.eql('Engineer');
+                person.job.company.should.eql('Foobar Inc.');
+                person.job.should.have.keys('title', 'company');
+                person.title.should.eql('Engineer');
+                done();
+            });
+        });
+
     });
-  });
+
+    describe('#create', function() {
+        it('builds and saves the object', function(done) {
+            Factory.create('job', function(err, job) {
+                (job instanceof Job).should.be.true;
+                job.title.should.eql('Engineer');
+                job.company.should.eql('Foobar Inc.');
+                job.saveCalled.should.be.true;
+                done();
+            });
+        });
+
+        it('passing attributes as second argument', function(done) {
+            Factory.create('job', { title: "Artist", company: "Bazqux Co." }, function(err, job) {
+                (job instanceof Job).should.be.true;
+                job.title.should.eql('Artist');
+                job.company.should.eql('Bazqux Co.');
+                job.saveCalled.should.be.true;
+                done();
+            });
+        });
+
+        it('Factory(...) instead of Factory.create(...)', function(done) {
+            Factory('job', function(err, job) {
+                (job instanceof Job).should.be.true;
+                job.title.should.eql('Engineer');
+                job.company.should.eql('Foobar Inc.');
+                job.saveCalled.should.be.true;
+                done();
+            });
+        });
+
+        it('when no model is defined then save is not supported', function(done) {
+            Factory.create('appointment', function(err, appointment) {
+                (err instanceof Error).should.be.true;
+                err.message.should.not.be.empty;
+                done();
+            });
+        });
+    });
+
+    describe('#object', function() {
+        it('returns a literal object', function(done) {
+            Factory.object('job', function(job) {
+                (job instanceof Job).should.not.be.true;
+                job.title.should.eql('Engineer');
+                job.company.should.eql('Foobar Inc.');
+                job.should.have.keys('title', 'company');
+                done();
+            });
+        });
+
+        it('passing attributes as second argument', function(done) {
+            var newTitle = 'oliebollenkraam uitbater';
+            Factory.object('job', { title: newTitle }, function(job) {
+                (job instanceof Job).should.not.be.true;
+                job.title.should.eql(newTitle);
+                job.company.should.eql('Foobar Inc.');
+                job.should.have.keys('title', 'company');
+                done();
+            });
+        });
+
+    });
+
 });
 
